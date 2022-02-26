ec429162c7517ecc536ed7482e5fbfef807d65fb
nikhil1-tripathi <tripathinikhil113@gmail.com>
2022-02-26 21:51:27 +0530

    Complete Lite and Pro pages .
    These pages found in the menu at the home page

---
+++ frontend/src/components/Lite/Lite.jsx
@@ -0,0 +1,146 @@
+import "./Lite.css"
+
+export function LitePage() {
+    return (
+        <div>
+            <div className="container_1">
+                <div className="box_1">
+
+                    <img className="inside_box1_1" src="https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2020/12/product-lite-01-header_2x.png" alt="" />
+
+                    <div className="inside_box1_2">
+                        <h3>PATREON LITE</h3>
+                        <h1 className="h1">Just the basics</h1>
+                        <p className="p">Simple tools to set up recurring support from your fans and run a membership business.</p>
+                        <button className="button1"> Get Started</button>
+                    </div>
+
+
+                </div>
+
+            </div>
+            <div className="container_2">
+                <div className="box_2">
+
+                    <img className="inside_box2_1_" src="https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2020/12/product-lite-02-features_2x.png"></img>
+
+                    <div className="inside_box2_2_">
+                        <div> <h1 id="black">Membership essentials</h1></div>
+                        <div className="box3_1" >
+                            <h2 className="box_3_2_">Hosted creator page</h2>
+                            <p className="black">Your creator page on patreon.com is where you invite fans to become patrons and post updates and content just for them.</p>
+
+                        </div>
+
+                        <div className="box3_1">
+                            <h2 className="box_3_2_">Patron communication tools</h2>
+                            <p className="black">You own the relationship with patrons. Communicate directly via email, posts on your page, or with direct messaging.</p>
+
+                        </div>
+                        <div className="box3_1">
+                            <h2 className="box_3_2_">Patreon workshops</h2>
+                            <p className="black">Learn from our Creator Success team about how to set up your page, what to offer, how to talk about Patreon, and more.</p>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+
+
+
+            <div id="container_3_">
+                <div className="box4_">
+                    <div className="box4_1_">
+                        <img className="box4_1_1_" src="https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2020/12/lite-avatar-bay-12_2x-1.png" ></img>
+                        <p className="box4_1_2_">TARN & ZACH , BAY 12 GAMES </p>
+                        <p className="box4_1_3_"> Video Game Creator</p>
+
+                    </div>
+                    <div className="box4_2_">
+                        Our patrons support us because they want us to keep releasing our video game. They’re not massively motivated by a huge list of benefits so we don’t need multiple tiers. We just offer a single level of support and patrons can choose the amount. This works great for us.
+
+                    </div>
+
+                </div>
+            </div>
+
+
+
+
+            <div id="container_4_">
+                <div id="box5_">
+                    <div id="box5_1_">
+                        <div id="box5_1_1_">All plans include:</div>
+                        <button id="button_">Compare All Plans</button>
+                        {/* <div className="box5_2_2_1"></div> */}
+                    </div>
+                    <div id="box5_2_">
+                        <div className="box5_2_1_">
+                            <div className="logo_"></div>
+                            <div className="headline_">Direct relationships</div>
+                            <div className="box5_2_2_1_">
+                                Develop relationships with your patrons  through patron-only posts, messages, or directly via email.
+                            </div>
+                        </div>
+                        <div className="box5_2_1_">
+                            <div className="logo_"></div>
+                            <div className="headline_">Business tools</div>
+                            <div className="box5_2_2_1_">
+                                Track patron payments, lifetime value, &nbsp; and  notes to help manage relationships with them directly.
+                            </div>
+                        </div>
+                        <div className="box5_2_1_">
+                            <div className="logo_"></div>
+                            <div className="headline_">Flexible payments</div>
+                            <div className="box5_2_2_1_">
+                                Pay yourself whenever you want, or receive monthly auto payouts from your creator balance via direct deposit, PayPal, or Payoneer.
+                            </div>
+                        </div>
+
+                        <div className="box5_2_2_">
+                            <div className="logo_"></div>
+                            <div className="headline_">Patron support</div>
+                            <div className="box5_2_2_1_">
+                                Rest easy knowing we handle your &nbsp; patrons’ questions, payments and &nbsp; concerns to ensure they’re taken care of.
+                            </div>
+                        </div>
+                        <div className="box5_2_2_">
+                            <div className="logo_"></div>
+                            <div className="headline_">Creator-first education</div>
+                            <div className="box5_2_2_1_">
+                                Learn how to effectively run your creative business with our workshops, world-class knowledge base and creator resources.
+                            </div>
+                        </div>
+                        <div className="box5_2_2_">
+                            <div className="logo_"></div>
+                            <div className="headline_">Mobile app</div>
+                            <div className="box5_2_2_1_">
+                                Message your patrons and monitor your membership business with our native iOS and Android apps.
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div id="container_5">
+                <div id="box6">
+                    <div id="box6_1">
+                        Join 200,000+ creators using Patreon today
+                    </div>
+                    <button id="btn"> Get Started With Lite </button>
+
+
+                </div>
+
+            </div>
+
+        </div>
+
+    )
+
+
+}
---
+++ frontend/src/components/Pro/Pro.jsx
@@ -0,0 +1,174 @@
+import "./Pro.css";
+
+export function StandardPage() {
+    return (
+        <div>
+            <div className="container1">
+                <div className="box1">
+
+                    <img className="inside_box1_1" src="https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2020/12/product-pro-01-header_2x.png" alt="" />
+
+                    <div className="inside_box1_2">
+                        <h3>PATREON LITE</h3>
+                        <p className="h1">All the tools to &nbsp; &nbsp; &nbsp; master membership</p>
+                        <p className="p">Build a thriving membership business that provides meaningful income for you and a rewarding experience for your patrons.</p>
+                        <button className="button1"> Get Started</button>
+                    </div>
+
+
+                </div>
+
+            </div>
+
+            <div className="container2">
+                <div className="box2">
+
+                    <img className="inside_box2_1" src="https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2020/12/product-pro-02-features-engage-your-patrons_2x.png"></img>
+
+                    <div className="inside_box2_2">
+                        <div> <h1 id="black">Engage your patrons</h1></div>
+                        <div className="box3_1" >
+                            <p className="box_3_2">Hosted creator page</p>
+                            <p className="black">Your creator page is where you invite fans to become patrons, showcase the benefits you offer, and post updates and content just for them.</p>
+
+                        </div>
+
+                        <div className="box3_1">
+                            <p className="box_3_2">Unlimited membership tiers</p>
+                            <p className="black">Membership tiers are levels of membership with different benefits and prices. We help you optimise your tiers and track and deliver benefits to patrons.</p>
+
+                        </div>
+                        <div className="box3_1">
+                            <p className="box_3_21">Patron communication tools</p>
+                            <p className="black">You own the relationship with patrons. You can communicate with them directly via email, posts on your page or with direct messaging.</p>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div id="container3">
+                <div className="container3_1">
+                    <p id="heading3_">Grow Your Bussiness</p>
+                    <p className="p3">
+                        <p className="p31">Special Offers promo tool</p>
+                        <p className="p32">Give yourself a raise! Offer something special for a limited time to get more patrons.</p>
+                    </p>
+                    <p className="p3">
+                        <p className="p31">Analytics and insights</p>
+                        <p className="p32">Easily check the health of your membership and uncover insights to help you grow and improve.</p>
+                    </p>
+                    <p className="p3">
+                        <p className="p31">Creator-led workshops</p>
+                        <p className="p32">Access our livestream workshops featuring top creators. Learn from creators in your field about how to build a creative career, both on and &nbsp;  &nbsp; off Patreon.</p>
+                    </p>
+                </div>
+                <div >
+                    <img className="container3_1" src="https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2020/12/product-pro-03-features-grow-your-business_2x.png" />
+                </div>
+            </div>
+
+            <div id="container4">
+                <div className="C4_1">
+                    <p id="C4_1_heading" >Connect your apps </p>
+                    <p id="C4_1_box">Patreon integrates with apps, like Discord, Crowdcast and more, that help you run your business, connect with patrons, deliver content and even automate tasks. </p>
+                    <button id="C4_1_btn"> See all apps</button>
+                </div>
+                <img className="C4_1" src="https://live-patreon-marketing.pantheonsite.io/wp-content/uploads/2020/12/pro-connect-apps-vimeo-updates.lg_2x.png" />
+            </div>
+
+
+            <div id="container6">
+                <div >
+                    <img id="video" src="./image.png" ></img>
+                </div>
+                <div id="C6_1_box">
+                    "I have all the autonomy to do whatever I want; I can go as fast or as slow as I want and can more or less say anything I want. Patreon gives me freedom and a much more genuine approach and relationship with the people who are following me."
+                </div>
+                <div id="C6_1_last">
+                    Simone Giertz, Shitty Robots
+                </div>
+            </div>
+
+
+
+
+            <div id="container_4">
+                <div id="box5">
+                    <div id="box5_1">
+                        <div id="box5_1_1">All plans include:</div>
+                        <button id="button">Compare All Plans</button>
+                        {/* <div className="box5_2_2_1"></div> */}
+                    </div>
+                    <div id="box5_2">
+                        <div className="box5_2_1">
+                            <div className="logo"></div>
+                            <div className="headline">Direct relationships</div>
+                            <div className="box5_2_2_1">
+                                Develop relationships with your patrons  through patron-only posts, messages, or directly via email.
+                            </div>
+                        </div>
+                        <div className="box5_2_1">
+                            <div className="logo"></div>
+                            <div className="headline">Business tools</div>
+                            <div className="box5_2_2_1">
+                                Track patron payments, lifetime value, &nbsp; and  notes to help manage relationships with them directly.
+                            </div>
+                        </div>
+                        <div className="box5_2_1">
+                            <div className="logo"></div>
+                            <div className="headline">Flexible payments</div>
+                            <div className="box5_2_2_1">
+                                Pay yourself whenever you want, or receive monthly auto payouts from your creator balance via direct deposit, PayPal, or Payoneer.
+                            </div>
+                        </div>
+
+
+
+                        <div className="box5_2_2">
+                            <div className="logo"></div>
+                            <div className="headline">Patron support</div>
+                            <div className="box5_2_2_1">
+                                Rest easy knowing we handle your &nbsp; patrons’ questions, payments and &nbsp; concerns to ensure they’re taken care of.
+                            </div>
+                        </div>
+                        <div className="box5_2_2">
+                            <div className="logo"></div>
+                            <div className="headline">Creator-first education</div>
+                            <div className="box5_2_2_1">
+                                Learn how to effectively run your creative business with our workshops, world-class knowledge base and creator resources.
+                            </div>
+                        </div>
+                        <div className="box5_2_2">
+                            <div className="logo"></div>
+                            <div className="headline">Mobile app</div>
+                            <div className="box5_2_2_1">
+                                Message your patrons and monitor your membership business with our native iOS and Android apps.
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+
+
+            <div id="container_5">
+                <div id="box6">
+                    <div id="box6_1">
+                        Join 200,000+ creators using Patreon today
+                    </div>
+                    <button id="btn"> Get Started With Lite </button>
+
+
+                </div>
+
+            </div>
+
+
+        </div>
+    )
+}
