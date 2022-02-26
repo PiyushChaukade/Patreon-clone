7c94722f2fc9195690490de0bbdf24fd01ece55e
PiyushChaukade <93372962+PiyushChaukade@users.noreply.github.com>
2022-02-27 00:43:25 +0530

    routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -81,6 +81,19 @@ import First from "./components/Firstpage/first"
 import { Createrpage } from './components/createpage/createrpage';
 import { Createrpage1 } from './components/createpage1/createrpage1';
 import {BrowserRouter} from 'react-router-dom'
+import { Createrpage2 } from './components/createpage2/createrpage2';
+import { Createrpage3 } from './components/createpage3/createrpage3';
+import { Createrpage4 } from './components/createrpage4/createrpage4';
+import { Basic } from './components/navbarpersonal/navbarcomponents/basic';
+import { Tiers } from './components/navbarpersonal/navbarcomponents/tiers';
+import { Merch } from './components/navbarpersonal/navbarcomponents/merch';
+import { Gettingpaid } from './components/navbarpersonal/navbarcomponents/gettingpaid';
+import { Pagesetting } from './components/navbarpersonal/navbarcomponents/pagesetting';
+import { Preview } from './components/navbarpersonal/navbarcomponents/preview';
+import { Advanced } from './components/navbarpersonal/navbarcomponents/advanced';
+// import Footer from './components/Footer/Footer';
+import { Creatorhome1 } from './components/navbarpersonal/navbarcomponents/Creator_home/Creatorhome1/Creatorhome1';
+
 function App() {
   const [user,setUser]=useState(null)
 
@@ -134,6 +147,22 @@ function App() {
          <Route path='/createpage' element={<Createrpage/>}> </Route>
          {/* <Route path='/create/adult' element={<Createrpage1/>}> </Route> */}
          <Route  path='/create/adult' element={<Createrpage1/>}></Route>
+         <Route  path='/create/currency' element={<Createrpage2/>}></Route>
+         <Route  path='/create/merch' element={<Createrpage3/>}></Route>
+         <Route  path='/create/socials' element={<Createrpage4/>}></Route>
+
+                  <Route  path='/create/navbar/edit/basic' element={<Basic/>}></Route>
+         <Route  path='/create/navbar/edit/tiers' element={<Tiers/>}></Route>
+          <Route  path='/create/navbar/edit/merch' element={<Merch/>}></Route>
+          <Route  path='/create/navbar/edit/gettingpaid' element={<Gettingpaid/>}></Route>
+         <Route  path='/create/navbar/edit/pagesetting' element={<Pagesetting/>}></Route>
+         <Route  path='/create/navbar/edit/preview' element={<Preview/>}></Route>        
+          <Route  path='/create/navbar/edit/advanced' element={<Advanced/>}></Route>
+         <Route  path='/create/navbar/edit/previewicon' element={<Preview/>}></Route>
+        
+         
+         <Route  path='/create-home' element={<Creatorhome1/>}></Route>
+
         
       </Routes>
       <Footer/>
